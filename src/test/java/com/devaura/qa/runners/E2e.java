87ab7d00bd81f402f34d65a235d412e30326598f
juanbig <juannodecoorp@gmail.com>
2024-11-13 08:02:30 -0500

    Finish Test Suite Vol +++

--- src/test/java/com/devaura/qa/runners/E2e.java
+++ src/test/java/com/devaura/qa/runners/E2e.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(plugin = { "pretty:target/cucumber/cucumber.txt",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "html:target/cucumber/report",
 		"json:target/cucumber/cucumber.json",
-		"com.devaura.qa.utils.TestListener" }, features = "resources/cucumbers/create_booking.feature", glue = "com.devaura.qa.definitions", monochrome = true, snippets = CAMELCASE
+		"com.devaura.qa.utils.TestListener" }, features = "resources/cucumbers", glue = "com.devaura.qa.definitions", monochrome = true, snippets = CAMELCASE
 // , dryRun = true // Use this to check for undefined steps without executing
 // tests
 // , publish = true // Use this to publish the report online
--- src/test/java/com/devaura/qa/runners/UpdateRunner.java
+++ src/test/java/com/devaura/qa/runners/UpdateRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "html:target/cucumber/report",
 		"json:target/cucumber/cucumber.json",
 		"com.devaura.qa.utils.TestListener" }, features = "resources/cucumbers/update.feature", glue = "com.devaura.qa.definitions", 
-		monochrome = true
+		monochrome = true, tags = "@upd-booking-dt"
 // , dryRun = true // Use this to check for undefined steps without executing
 // tests
 // , publish = true // Use this to publish the report online
